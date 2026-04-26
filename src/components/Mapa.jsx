a0e1a0c0d57d42c479e8c4619016334e9dfed915
ArielGonzalez95 <gonzalez.r.ariel@gmail.com>
2026-04-26 20:19:55 -0300

    Mpa nuevo

--- src/components/Mapa.jsx
+++ src/components/Mapa.jsx
@@ -4,7 +4,7 @@ export default function Mapa() {
       <h3>ENCONTRANOS EN</h3>
       <iframe
         loading="lazy"
-        src="https://maps.google.com/maps?q=EIPK%2C%20Escuela%20Integral%20de%20Parkour&t=m&z=15&output=embed&iwloc=near"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3284.172258186147!2d-58.4444326!3d-34.5998054!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcca09b47f77cb%3A0xf15a75abf9bdcf9c!2sParkour%20Integral%20(Escuela%20Integral%20de%20Parkour)!5e0!3m2!1ses!2sar!4v1777245543563!5m2!1ses!2sar"
         title="EIPK, Escuela Integral de Parkour"
         aria-label="EIPK, Escuela Integral de Parkour"
       />
